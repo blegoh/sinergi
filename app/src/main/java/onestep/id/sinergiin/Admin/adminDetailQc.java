42ba9acea54d7d9f8abeb64c15122b5348639028
Brian Rizqi <30972350+brianrizqi@users.noreply.github.com>
2018-09-27 18:10:19 +0700

    bug fixed

--- app/src/main/java/onestep/id/sinergiin/Admin/adminDetailQc.java
+++ app/src/main/java/onestep/id/sinergiin/Admin/adminDetailQc.java
@@ -3,6 +3,8 @@ package onestep.id.sinergiin.Admin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import onestep.id.sinergiin.R;
+
 public class adminDetailQc extends AppCompatActivity {
 
     @Override
