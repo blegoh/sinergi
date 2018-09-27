44e4014cea664d42e3230e80637613033f232b64
abbinizar <abbinizarm@gmail.com>
2018-09-27 13:09:23 +0700

    layout tambahan

---
+++ app/src/main/java/onestep/id/sinergiin/pengrajinDetailPembelian.java
@@ -0,0 +1,13 @@
+package onestep.id.sinergiin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pengrajinDetailPembelian extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pengrajin_detail_pembelian);
+    }
+}
---
+++ app/src/main/java/onestep/id/sinergiin/pengrajinDetailQc.java
@@ -0,0 +1,13 @@
+package onestep.id.sinergiin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pengrajinDetailQc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pengrajin_detail_qc);
+    }
+}
